cc803e12009be0029faae69496a3920f37100747
Jonas Samuelsson <jonassamuelsson@outlook.com>
2020-03-25 17:06:31 +0100

    added handyman-docs app & test projects

---
+++ src/Handyman.Tools.Docs/src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Handyman.Tools.Docs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
